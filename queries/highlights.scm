21c3892e6cf3c8f380d59b4166af12ae776feca8
Vincent Rischmann <vincent@rischmann.fr>
2023-07-21 16:29:55 +0200

    update highlights

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,6 +1,15 @@
 (component_declaration
   name: (component_identifier) @function)
 
+(open_tag
+  name: (element_identifier) @tag)
+(close_tag
+  name: (element_identifier) @tag)
+
+(attribute
+  name: (attribute_name) @attribute
+  value: (attribute_value) @string)
+
 [
   "package"
   "templ"
