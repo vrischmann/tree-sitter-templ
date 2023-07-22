6b609aaa5f0f2a7d53975353d1e518486f6d71ee
Vincent Rischmann <vincent@rischmann.fr>
2023-07-22 14:20:14 +0200

    queries: update highlights

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,3 +1,26 @@
+; TODO(vincent): can we use injection ? I can't get it working
+
+; A bunch of these are taken from:
+; * https://github.com/nvim-treesitter/nvim-treesitter/blob/master/queries/go/highlights.scm
+; * https://github.com/tree-sitter/tree-sitter-go/blob/master/queries/highlights.scm
+
+(package_identifier) @namespace
+
+(parameter_declaration (identifier) @parameter)
+(variadic_parameter_declaration (identifier) @parameter)
+
+(function_declaration
+  name: (identifier) @function)
+
+(type_spec name: (type_identifier) @type.definition)
+(type_identifier) @type
+(field_identifier) @property
+(identifier) @variable
+
+;
+; These are Templ specific
+;
+
 (component_declaration
   name: (component_identifier) @function)
 
@@ -15,8 +38,30 @@
 (expression) @function.method
 
 [
-  "package"
+  "func"
+  "var"
+  "const"
   "templ"
   "css"
   "type"
+  "return"
+  "struct"
 ] @keyword
+
+[
+  "import"
+  "package"
+] @include
+
+[
+  "else"
+  "case"
+  "switch"
+  "if"
+] @conditional
+
+[
+  (interpreted_string_literal)
+  (raw_string_literal)
+  (rune_literal)
+] @string
